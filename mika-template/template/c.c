c4be853d7dc362a9168f4f77c91a1264ed2b2046
iluvgirlswithglasses <luunamdat204@gmail.com>
2021-11-16 19:23:21 +0700

    c template!

---
+++ mika-template/template/c.c
@@ -0,0 +1,17 @@
+
+/*
+author: 	iluvgirlswithglasses 
+github: 	https://github.com/iluvgirlswithglasses 
+created:	Tue Nov 16 19:22:24 2021
+tab-width:	4 spaces
+
+I sexually identify myself as an Attack Helicopter
+BTW I use Arch
+*/
+
+#include <stdio.h>
+
+int main() {
+	return 0;
+}
+
---
+++ mika-template/template/t.c
@@ -0,0 +1,17 @@
+
+/*
+author: 	iluvgirlswithglasses 
+github: 	https://github.com/iluvgirlswithglasses 
+{{date}}
+tab-width:	4 spaces
+
+I sexually identify myself as an Attack Helicopter
+BTW I use Arch
+*/
+
+#include <stdio.h>
+
+int main() {
+	return 0;
+}
+
